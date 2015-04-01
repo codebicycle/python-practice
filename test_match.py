4af1253c9d2452408b8a0d3e009f8bd080ce1ab4
Adrian Damian <damian.adrian24@gmail.com>
2015-04-02 02:31:41 +0300

    Added empty match.py and basic tests.

---
+++ test_match.py
@@ -0,0 +1,61 @@
+"""
+match.py is a grep-like utility.
+
+Usage: match.py [OPTION]... PATTERN [FILE]...
+
+See the docstring of each test_* function for specific requirements.
+
+"""
+
+from subprocess import Popen, PIPE
+import os.path
+import sys
+
+HERE = os.path.dirname(__file__)
+
+
+def call(*args, **kwargs):
+    """Call match.py with the arguments in args. If the input keyword argument
+    is given, send it to stdin. Return a (stdoutdata, stderrdata, returncode)
+    tuple.
+
+    """
+    command = (sys.executable, os.path.join(HERE, 'match.py')) + args
+    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate(kwargs.get('input'))
+    return out, err, p.returncode
+
+
+def test_basic(tmpdir):
+    """Search the named input FILEs (or standard input if no files are named)
+    for lines matching the given PATTERN (substring match). By default, print
+    the matching lines.
+
+    """
+    tmpdir.chdir()
+    tmpdir.join('one').write('one\n')
+    tmpdir.join('two').write('two\n')
+    assert call('o', input='zero\n') == ('zero\n', '', 0)
+    assert call('o', 'one', input='zero\n') == ('one\n', '', 0)
+    assert call('o', 'one', 'two') == ('one\ntwo\n', '', 0)
+
+
+def test_status(tmpdir):
+    """Exit with status 0 if any matches are found and with 1 if not.
+    Exit with status 2 in case of error.
+
+    """
+    tmpdir.chdir()
+    assert call('o', input='one\n') == ('one\n', '', 0)
+    assert call('x', input='one\n') == ('', '', 1)
+
+    # three doesn't exist
+    out, err, status = call('x', 'three')
+    assert out == ''
+    assert status == 2
+
+    # no pattern is given
+    out, err, status = call()
+    assert out == ''
+    assert status == 2
+
