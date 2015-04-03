148e0a23bd91cbc3533d44c3a5c46a471e97d8b7
codebicycle <codebicycle@gmail.com>
2015-04-03 10:06:54 +0300

    Revert "universal_newlines=True added to test script"
    This reverts commit 759aa86c5f8ddef54ba30bd6bb0df48556a917a1.

--- test_match.py
+++ test_match.py
@@ -21,7 +21,7 @@ def call(*args, **kwargs):
 
     """
     command = (sys.executable, os.path.join(HERE, 'match.py')) + args
-    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    p = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate(kwargs.get('input'))
     return out, err, p.returncode
 
