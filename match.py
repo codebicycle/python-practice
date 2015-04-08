5d56fd36aa3f3cb60cb7d0fb24c92fb818fc1a19
codebicycle <codebicycle@gmail.com>
2015-04-08 22:03:42 +0300

    test only_matching passes. Naive command line options read

--- match.py
+++ match.py
@@ -2,27 +2,57 @@
 
 import sys
 
-def is_matched(pattern, file):
+def opts_and_args(argv, supported_options):
+    args = argv[1:]
+    active_options = [opt for opt in supported_options if opt in args]
+
+    for opt in active_options:
+        while opt in args:
+            args.remove(opt)
+
+    return(active_options, args)
+
+
+def is_matched(pattern, file, output_cb):
     matched = False
     for line in file:
         if pattern in line:
             matched = True
-            print(line.decode().rstrip('\n'))
+            output_cb(pattern, line)
 
     return matched
 
 
+def default_output(pattern, line):
+    print(line.decode().rstrip('\n'))
+
+
+def only_matching_output(pattern, line):
+    if not pattern:
+        return
+    for i in range(line.count(pattern)):
+        print(pattern.decode())
+
+
 if __name__ == "__main__":
 
+    SUPPORTED_OPTIONS = ['-o', '--only-matching']
+    options, args = opts_and_args(sys.argv, SUPPORTED_OPTIONS)
+
     try:
-        pattern = sys.argv[1].encode('utf-8')
+        pattern = args[0].encode('utf-8')
     except IndexError:
         exit(2)
 
-    filenames = sys.argv[2:]
+    filenames = args[1:]
+
+    if '-o' in options or '--only-matching' in options:
+      cb = only_matching_output
+    else:
+      cb = default_output
 
     if len(filenames) < 1:
-        matched = is_matched(pattern, sys.stdin.buffer)
+        matched = is_matched(pattern, sys.stdin.buffer, cb)
         if not matched:
             exit(1)
     else:
@@ -33,6 +63,6 @@ if __name__ == "__main__":
                 exit(2)
             else:
                 with  f:
-                    matched = is_matched(pattern, f)
+                    matched = is_matched(pattern, f, cb)
         if not matched:
             exit(1)
