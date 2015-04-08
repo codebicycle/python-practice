8649cf43d48ba0c9ec0c3e7038f11417402a7db8
codebicycle <codebicycle@gmail.com>
2015-04-08 20:00:57 +0300

    small refactoring
    1. separate function is_matched()
    2. pattern spelled correctly

--- match.py
+++ match.py
@@ -2,25 +2,30 @@
 
 import sys
 
+def is_matched(pattern, file):
+    matched = False
+    for line in file:
+        if pattern in line:
+            matched = True
+            print(line.decode().rstrip('\n'))
+
+    return matched
+
+
 if __name__ == "__main__":
 
     try:
-        patern = sys.argv[1].encode('utf-8')
+        pattern = sys.argv[1].encode('utf-8')
     except IndexError:
         exit(2)
 
     filenames = sys.argv[2:]
 
     if len(filenames) < 1:
-        match = False
-        for line in sys.stdin.buffer:
-            if patern in line:
-                match = True
-                print(line.decode().rstrip('\n'))
-        if not match:
+        matched = is_matched(pattern, sys.stdin.buffer)
+        if not matched:
             exit(1)
     else:
-        match = False
         for filename in filenames:
             try:
                 f = open(filename, mode="rb")
@@ -28,9 +33,6 @@ if __name__ == "__main__":
                 exit(2)
             else:
                 with  f:
-                    for line in f:
-                        if patern in line:
-                            match = True
-                            print(line.decode().rstrip('\n'))
-        if not match:
+                    matched = is_matched(pattern, f)
+        if not matched:
             exit(1)
