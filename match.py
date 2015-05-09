6fed31409c8b805839d33a4c749b2487dd53200e
codebicycle <codebicycle@gmail.com>
2015-05-09 11:47:48 +0300

    Add option -e --regexp

--- match.py
+++ match.py
@@ -30,23 +30,76 @@ def iter_files(paths):
     '-i', '--ignore-case', is_flag=True,
     help=('ignore case distinctions')
 )
-@click.argument('pattern')
+@click.option(
+    '-e', '--regexp',
+    help=('use PATTERN for matching')
+)
+@click.argument('pattern', required=False)
 @click.argument('file', nargs=-1, type=click.Path(exists=True))
-def main(pattern, file, **kwargs):
+def match(pattern, file, **kwargs):
     '''Search for PATTERN in each FILE or standard input.'''
-    # print(kwargs)
-    # print('pattern:', pattern)
-    # print('file:', file)
+    # print('match: pattern:', pattern)
+    # print('match: file:', file)
+    # print('match: kwargs:', kwargs)
+
+    if kwargs['regexp']:
+        match_regexp()
+    else:
+        match_helper(pattern, file, **kwargs)
+
+
+@click.command()
+@click.option(
+    '-o', '--only-matching', is_flag=True,
+    help=('show only the part of a line matching PATTERN')
+)
+@click.option(
+    '-h', '--no-filename', is_flag=True,
+    help=('suppress the file name prefix on output')
+)
+@click.option(
+    '-v', '--invert-match', is_flag=True,
+    help=('select non-matching lines')
+)
+@click.option(
+    '-i', '--ignore-case', is_flag=True,
+    help=('ignore case distinctions')
+)
+@click.option(
+    '-e', '--regexp', multiple=True,
+    help=('use PATTERN for matching')
+)
+@click.argument('file', nargs=-1, type=click.Path(exists=True))
+def match_regexp(file, **kwargs):
+    # print('match-regexp: file:', file)
+    # print('match-regexp: kwargs:', kwargs)
+
+    pattern = kwargs['regexp']
+    match_helper(pattern, file, **kwargs)
+
+
+def match_helper(pattern, file, **kwargs):
+    # print('match_helper: pattern:', pattern)
+    # print('match_helper: file:', file)
+    # print('match_helper: kwargs:', kwargs)
+
+    if pattern is None:
+        sys.exit(2)
+
+    opt_no_filename = kwargs.get('no_filename')
+    opt_only_matching = kwargs.get('only_matching')
+    opt_ignore_case = kwargs.get('ignore_case')
+    opt_invert_match = kwargs.get('invert_match')
 
     def output():
-        if kwargs['no_filename'] or len(file) <= 1:
+        if opt_no_filename or len(file) <= 1:
             filename = b''
         else:
             filename = '{}:'.format(f.name).encode('utf8')
 
-        if kwargs['only_matching']:
-            if pattern:
-                for m in re.findall(pattern, line, flag):
+        if opt_only_matching:
+            if pat:
+                for m in re.findall(pat, line, flag):
                     out = filename + m + b'\n'
                     sys.stdout.buffer.write(out)
         else:
@@ -54,28 +107,37 @@ def main(pattern, file, **kwargs):
             sys.stdout.buffer.write(out)
 
 
-    pattern = pattern.encode('utf8')
-    flag = re.IGNORECASE if kwargs['ignore_case'] else 0
+    if type(pattern) is tuple:
+        patterns = [pat.encode('utf8') for pat in pattern[::-1]] 
+    else:
+        patterns = [pattern.encode('utf8')]
 
     if file:
         files = iter_files(file)
     else:
         files = (sys.stdin.buffer,)
 
+    flag = re.IGNORECASE if opt_ignore_case else 0
+
     matched = False
     for f in files:
         for line in f:
-            match = re.search(pattern, line, flag)
-            if kwargs['invert_match']:
-                if not match:
+            for pat in patterns:
+                found = re.search(pat, line, flag)
+                if opt_invert_match:
+                    if not found:
+                        output()
+                        matched = True
+                elif found:
                     output()
                     matched = True
-            elif match:
-                output()
-                matched = True
         f.close()
     if not matched:
         sys.exit(1)
+
+
+def main():
+    match()
 
 
 if __name__ == "__main__":
